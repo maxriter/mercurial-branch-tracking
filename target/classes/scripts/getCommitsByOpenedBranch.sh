7c64fe4124eff888132b6f4663e9d26012918b1d
Maxim Lysenko <lysenko.mk@gmail.com>
2017-08-20 23:16:47 +0300

    removed unused files

--- target/classes/scripts/getCommitsByOpenedBranch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-PROJECT_PATH=$1
-BRANCH_NAME=$2
-
-cd ~/branch-tracking-projects/$PROJECT_PATH && hg log -r "branch($BRANCH_NAME)" --limit 30
--- target/classes/scripts/getOpenedBranches.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PROJECT_PATH=$1
-
-cd ~/branch-tracking-projects/$PROJECT_PATH && hg branches -q
